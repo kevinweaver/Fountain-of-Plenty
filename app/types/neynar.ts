203c0b388ccb26633829df45c6b0b8255fb23423
Kevin Weaver <kevinweaver2@gmail.com>
2024-05-04 09:03:51 -0700

    Add neynar types

---
+++ app/types/neynar.ts
@@ -0,0 +1,28 @@
+export type User = NeynarUser & {
+  address: string;
+  isActive: boolean;
+};
+
+export type NeynarUser = {
+  object: "user";
+  fid: number;
+  custody_address: string;
+  username: string;
+  display_name: string;
+  pfp_url: string;
+  profile: {
+    bio: {
+      text: string;
+      mentioned_profiles: [];
+    };
+  };
+  follower_count: number;
+  following_count: number;
+  verifications: string[];
+  verified_addresses: {
+    eth_addresses: string[];
+    sol_addresses: string[];
+  };
+  active_status: "active" | "inactive";
+  power_badge: boolean;
+};
