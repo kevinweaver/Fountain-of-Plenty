0c2992b565bc8c979c5424f10603818af3178d24
Kevin Weaver <kevinweaver2@gmail.com>
2024-05-04 09:04:17 -0700

    Add fountain types

---
+++ app/types/fountain.ts
@@ -0,0 +1,57 @@
+import { FrameContext } from "frog";
+import { Address, Hex } from "viem";
+
+///
+////// FRAME TYPES
+///
+
+export const steps = ["init", "name", "symbol", "meme"] as const;
+
+export type Step = (typeof steps)[number];
+
+export type FountainState = {
+  frameState: "init" | "ready";
+  draftValues?: {
+    name?: string;
+    symbol?: string;
+  };
+};
+
+export const initialFountainState: FountainState = {
+  frameState: "init",
+} as const;
+
+export type FountainContext = FrameContext<{
+  State: FountainState;
+}>;
+
+///
+////// REDIS TYPES
+///
+
+export type FountainToken = {
+  name: string;
+  symbol: string;
+  tokenUrl: string;
+  memeCastHash: Hex;
+  tokenAddress: Address;
+};
+
+export type FountainDraftToken = {
+  /**
+   * ISO Date string
+   */
+  updatedTimestamp: string;
+  name: string;
+  symbol: string;
+  /**
+   * undefined if they haven’t posted
+   * their meme yet
+   */
+  memeCastHash?: Hex;
+};
+
+export type FountainUser = {
+  draftToken?: FountainDraftToken;
+  deployedTokens: FountainToken[];
+};
